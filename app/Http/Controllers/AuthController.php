acde867179bc057912676b00ea5f4ba5457904c4
Cristian Gonzalez <cristiangt9@gmail.com>
2021-04-30 17:26:39 -0500

    Eliminacion de migraciones innecesarias y declaracion del modelo transaccion y su migracion

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * Clase usada para servir las funciones de este servicio "User"
+ */
+class AuthController
+{
+    public function register($documento, $nombres, $email, $calular)
+    {
+        
+    }
+}
